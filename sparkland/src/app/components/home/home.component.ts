6c3870521c4c3cf86b304a63eafe4d590324c856
Famous <jfamousket@gmail.com>
2018-07-29 02:58:24 +0100

    changed icon set
    removed micons and now using font awesome
    removed scrollmagic and gsap

--- sparkland/src/app/components/home/home.component.ts
+++ sparkland/src/app/components/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlCarousel } from 'ngx-owl-carousel';
-// import { TweenMax } from 'gsap';
-// import * as ScrollMagic from 'ScrollMagic';
-// import 'ScrollMagic/scrollmagic/minified/plugins/debug.addIndicators.min.js';
 import * as $ from 'jquery';
 
 @Component({
@@ -16,22 +13,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // $('.service, .team').each(function () {
-    //   const controller = new ScrollMagic.Controller();
-
-    //   const fadeInDown = new ScrollMagic.Scene({
-    //     triggerElement: this,
-    //     triggerHook: 0.85,
-    //     reverse: true
-    //   })
-    //   .setClassToggle(this, 'animFadeInDown')
-    //   .addIndicators({
-    //     name: 'fadeScene',
-    //     colorTrigger: 'black',
-    //     colorStart: '#75c695'
-    //   })
-    //   .addTo(controller);
-    // });
   }
 
 }
